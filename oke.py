a93c4f67c6062a3efa81acbb7a30d78215dc03b8
lercpp <flashiap08@gmail.com>
2025-11-01 20:25:50 +0300

    oke

--- oke.py
+++ oke.py
@@ -3,9 +3,14 @@ import tkinter as tk
 def calculate():
     try:
         result = str(eval(entry.get()))
+        entry.delete(0, tk.END)  
         entry.insert(0, result)
     except:
+        entry.delete(0, tk.END)  
         entry.insert(0, "Ошибка")
+
+def clear():
+    entry.delete(0, tk.END)
 
 root = tk.Tk()
 root.title("pik me")
@@ -18,15 +23,19 @@ but = [
     '7', '8', '9', '/',
     '4', '5', '6', '*',
     '1', '2', '3', '-',
-    '0', '.', '=', '+'
+    '0', '.', '=', '+',
+    'C', 'KA', 'PA', '!'  
 ]
 
 row_v = 1
 col_v = 0
 
 for button in but:
-    tk.Button(root, text=button, padx=20, pady=20,
-              command=lambda b=button: entry.insert(tk.END, b) if b != '=' else calculate()).grid(row=row_v, column=col_v)
+    if button == 'C':
+        tk.Button(root, text=button, padx=20, pady=20, command=clear).grid(row=row_v, column=col_v)
+    else:
+        tk.Button(root, text=button, padx=20, pady=20,
+                  command=lambda b=button: entry.insert(tk.END, b) if b != '=' else calculate()).grid(row=row_v, column=col_v)
     col_v += 1
     if col_v > 3:
         col_v = 0
